dc03e4c12b1c0620e0b456a4db49ccd0e96744f4
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-09-02 02:14:26 +0530

    Time: 442 ms (50.93%), Space: 17.1 MB (72.45%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -3,16 +3,15 @@ class Solution:
         l = nz = 0
         count = maxc = 0
         
-
         for r in range(len(nums)):
             if nums[r] == 0:
                 nz += 1
-            
+
             while nz > k:
                 if nums[l] == 0:
                     nz-=1 
                 l+=1
-            
+                
             count = r-l+1
             maxc = max(maxc,count)
            
