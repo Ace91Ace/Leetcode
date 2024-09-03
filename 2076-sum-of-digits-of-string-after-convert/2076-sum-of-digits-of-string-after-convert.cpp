83ee0b12ef270267b9d263efff448e142ada16e9
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-09-03 22:34:39 +0530

    Time: 5 ms (25.74%), Space: 10.1 MB (21.06%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -6,16 +6,15 @@ public:
         for (int i = 0; i < s.size(); i++){
             ascii.append(to_string(static_cast<int>(s[i] - 'a' + 1)));
         }
-        cout<<ascii<<endl;
+        
         for (int i = 0; i < k ; i++){
             int sum = 0;
             for (int j = 0; j < ascii.size(); j++){
-                sum += ascii[j] - '0'; // subtract '0' to get the integer value of the digit
+                sum += ascii[j] - '0'; 
             }
             ascii = to_string(sum);
         }
         
-        cout<<ascii<<endl;
-        return stoi(ascii); // return the final integer value
+        return stoi(ascii); 
     }
 };
