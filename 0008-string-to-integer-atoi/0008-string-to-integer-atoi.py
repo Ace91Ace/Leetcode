d22d6d0f72511f79d1549bfdd3d67600493ea009
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-09-08 22:58:57 +0530

    Time: 36 ms (69.98%), Space: 16.7 MB (20.36%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,18 @@
+class Solution:  
+    def myAtoi(self, s: str) -> int:  
+        num, sign, index = [], 1, 0  
+        
+        while index < len(s) and s[index] == ' ':  
+            index += 1  
+        if index < len(s) and s[index] in ['-', '+']:  
+            sign = -1 if s[index] == '-' else 1  
+            index += 1  
+        while index < len(s) and s[index].isdigit():  
+            num.append(int(s[index]))  
+            index += 1  
+            
+        if not num:  
+            return 0  
+        
+        result = sign * reduce(lambda x, y: 10 * x + y, num)  
+        return max(min(result, 2**31 - 1), -2**31) 
