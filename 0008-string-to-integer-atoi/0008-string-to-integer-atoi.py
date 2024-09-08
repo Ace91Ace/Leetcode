2325b76d658fb5718dbcc8c22c5e78e639d14c6e
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-09-08 23:18:10 +0530

    Time: 36 ms (69.98%), Space: 16.7 MB (20.36%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -2,8 +2,7 @@ class Solution:
     def myAtoi(self, s: str) -> int:  
         num, sign, index = [], 1, 0  
         
-        while index < len(s) and s[index] == ' ':  
-            index += 1  
+        s = s.strip()
         if index < len(s) and s[index] in ['-', '+']:  
             sign = -1 if s[index] == '-' else 1  
             index += 1  
