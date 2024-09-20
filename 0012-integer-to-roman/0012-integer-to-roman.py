69c515d1e482c89fae086a208a53ccd4b59ff88e
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-09-20 23:40:29 +0530

    Time: 46 ms (62.18%), Space: 16.6 MB (17.3%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intToRoman(self, num: int) -> str:
+        """
+        :type num: int
+        :rtype: str
+        """
+        symlist = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50],
+                   ["XC", 90], ["C", 100], ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]]
+        res = ""
+        for sym, val in reversed(symlist):
+            if num // val:
+                count = num // val
+                res += (sym * count)
+                num = num % val
+        return res
