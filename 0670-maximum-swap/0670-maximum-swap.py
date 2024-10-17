b768fce80d28a31ff473c4d0d540b7eba4ee3b30
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-10-18 00:11:43 +0530

    Time: 0 ms (100%), Space: 16.5 MB (79.76%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,13 +1,11 @@
 class Solution:  
     def maximumSwap(self, num: int) -> int:  
         num = list(str(num))  
-        x = sorted(num)[::-1]  # sorted in descending order  
+        x = sorted(num)[::-1]  
 
         for i in range(len(num)):  
             if num[i] != x[i]:  
-                # Find the last occurrence of the character to swap  
-                y = len(num) - 1 - num[::-1].index(x[i])  
-                # Swap the characters  
+                y = len(num) - 1 - num[::-1].index(x[i]) 
                 num[i], num[y] = num[y], num[i]  
                 break  
 
