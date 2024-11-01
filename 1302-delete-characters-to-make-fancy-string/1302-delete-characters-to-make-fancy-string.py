e1676158e926251e98ff2e7786c61ef012eed35b
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-01 15:58:30 +0530

    Time: 293 ms (68.47%), Space: 19 MB (8.28%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -6,11 +6,9 @@ class Solution:
         flag = 0
         for i in range(1,len(s)):
             if s[i] == c and flag == 0:
-                print(s[i])
                 x.append(s[i])
                 flag = 1
             elif s[i] != c:
-                print(s[i])
                 x.append(s[i])
                 flag = 0
             c = x[-1]
