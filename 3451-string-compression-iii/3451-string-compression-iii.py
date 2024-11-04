7c793e3b33fd37f91307eb2c9e74a0be16142d0d
Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-04 16:40:03 +0530

    Time: 155 ms (86.53%), Space: 28.2 MB (12.58%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.py
@@ -0,0 +1,29 @@
+class Solution:  
+    def compressedString(self, word: str) -> str:  
+        if not word:  
+            return ""  
+        
+        curr = word[0]  
+        l = 1  
+        x = []  
+        
+        for i in range(1, len(word)):  
+            if curr == word[i]:  
+                l += 1  
+            else:
+                if l > 0:  
+                    x.append(str(l))  
+                    x.append(curr)  
+                curr = word[i]  
+                l = 1  
+            
+            if l == 9:  
+                x.append(str(l))  
+                x.append(curr)  
+                l = 0   
+
+        if l > 0:  
+            x.append(str(l))  
+            x.append(curr)  
+
+        return ''.join(x)
