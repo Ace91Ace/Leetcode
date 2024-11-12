7a569bfc741dda6b48f88d56db69cb745b8efa91
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-12 21:52:43 +0530

    Time: 0 ms (100%), Space: 16.7 MB (38.25%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,32 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        n = 0
+        md = 1
+        if (dividend < 0):
+            md = -md
+            dividend = abs(dividend)
+        if (divisor < 0):
+            md = -md
+            divisor = abs(divisor)
+        nn = 1
+        dvd = dividend
+        dvr = divisor
+        sn = 0
+        while (dvd >= dvr):
+            nn = 1
+            dd = dvr
+            while (dvd >= dd):
+                sn += nn
+                dvd -= dd
+                dd += dd
+                nn += nn
+
+        ans = sn
+
+        if (md < 0):
+            ans = -ans
+        if (ans > 2 ** 31 - 1):
+            ans = 2 ** 31 - 1
+        if (ans < -2 ** 31):
+            ans = -2 ** 31
+        return (ans)
