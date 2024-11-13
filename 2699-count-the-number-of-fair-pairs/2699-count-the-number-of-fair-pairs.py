871b1ccbe00a687d0f95d46c41ff032db7089a61
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-14 00:07:35 +0530

    Time: 1007 ms (23.81%), Space: 30.5 MB (91.73%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -3,12 +3,10 @@ class Solution:
         nums.sort()  
         count = 0  
         n = len(nums)  
-        
          
         for left in range(n):  
             low = left + 1 
             high = n  
-             
             while low < high:  
                 mid = (low + high) // 2  
                 if nums[left] + nums[mid] > upper:  
@@ -17,7 +15,6 @@ class Solution:
                     low = mid + 1  
             right_high = low  
             
-           
             low = left + 1  
             high = n  
             
@@ -27,9 +24,7 @@ class Solution:
                     low = mid + 1  
                 else:  
                     high = mid  
-            
             right_low = low  
-            
             count += (right_high - right_low)  
         
         return count
