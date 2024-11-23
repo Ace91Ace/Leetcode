c3f9658492db0bdad57674b0f6e561bd7104a2de
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-23 20:09:07 +0530

    Time: 1854 ms (89.87%), Space: 29 MB (65.69%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -1,16 +1,18 @@
-class Solution:  
-    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:  
-        for i in box:  
-            start = 0  
-            for j in range(len(i)):  
-                if i[j] == '*':  
-                    i[start:j] = sorted(i[start:j], reverse=True)  
-                    start = j + 1  
-            i[start:] = sorted(i[start:], reverse=True)  
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        for i in box:
+            start = 0
+            for j in range(len(i)):
+                if i[j] == '*':
+                    i[start:j] = sorted(i[start:j] , reverse = True)
+                    start = j + 1
+            i[start:] = sorted(i[start:], reverse = True)
 
-        def rotate(box):  
-            tran = [list(row) for row in zip(*box)]  
-            rot = [row[::-1] for row in tran]  
-            return rot  
+        def rotate(box):
+            tran = [list(row) for row in zip(*box)]
+            rot = [row[::-1] for row in tran]
+            return rot
         
-        return rotate(box) 
+        return rotate(box)
+        
+        
