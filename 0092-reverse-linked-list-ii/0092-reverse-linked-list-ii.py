5857f3511c59560fcd31817fadd5a1ff52df31c6
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-24 23:03:00 +0530

    Time: 0 ms (100%), Space: 16.7 MB (35.41%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -4,7 +4,6 @@ class Solution:
         l = r = None
         curr = head
 
-        # Identify nodes before `left` and after `right`
         while curr:
             if count == left - 1:
                 l = curr
@@ -13,7 +12,6 @@ class Solution:
             curr = curr.next
             count += 1
 
-        # If `left` is the head, adjust `l`
         if not l:
             l = ListNode(0)
             l.next = head
@@ -23,7 +21,6 @@ class Solution:
         y = l.next
         temp = y
 
-        # Reverse the sublist
         while temp != r:
             temp = y.next
             y.next = x
@@ -32,7 +29,6 @@ class Solution:
 
         l.next = x
 
-        # If `left` was the head, skip the placeholder node
         if l.val == 0:
             return l.next
         return head
