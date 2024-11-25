fa37b120b34499b2a7077e5d3893ef277763615e
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-26 03:56:17 +0530

    Time: 0 ms (100%), Space: 16.6 MB (48%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,15 +1,14 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        dit = {'1':1 ,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'0':0 }
         n1 = n2 = 0
-
+        
         num1 = num1[::-1]
         for i in range(len(num1)):
-            n1 = n1 + (10**(i))*dit[num1[i]]
+            n1 = n1 + (10**(i))*(ord(num1[i])-48)
 
         num2 = num2[::-1]
         for i in range(len(num2)):
-            n2 = n2 + (10**(i))*dit[num2[i]]
+            n2 = n2 + (10**(i))*(ord(num2[i])-48)
         
         return str(n1*n2)
         
