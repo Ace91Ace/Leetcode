fa9062d2827382f1c6dd106db4ea7a7613e602f4
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-11-26 10:29:37 +0530

    Time: 7 ms (68.21%), Space: 17.7 MB (80.31%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -1,36 +1,16 @@
-from typing import List
-
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        if edges == []:
-            return 0 if n == 1 else -1
+        tab = [0]*n
+        c = 0
+        w = -1
 
-        x = set()
-        y = set()
-
-        for i in edges:
-            x.add(i[0])
-            y.add(i[1])
+        for i,j in edges:
+            tab[j] = -1
         
-        w = list(x.difference(y))
-        
-        if len(w) != 1:
-            return -1
-        
-        candidate = w[0]
-        
-        reachable = set()
-        stack = [candidate]
-        
-        while stack:
-            node = stack.pop()
-            if node not in reachable:
-                reachable.add(node)
-                for edge in edges:
-                    if edge[0] == node and edge[1] not in reachable:
-                        stack.append(edge[1])
-        
-        if len(reachable) == n:
-            return candidate
-        
+        for i in range(n):
+            if tab[i] == 0:
+                w = i
+                c += 1
+        if c == 1:
+            return w
         return -1
