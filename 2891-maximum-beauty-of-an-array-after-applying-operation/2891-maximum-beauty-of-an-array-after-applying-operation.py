44d56d1617cefa6ac98bc59076f8e9502fc1eaf2
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-12-12 00:57:29 +0530

    Time: 783 ms (15.38%), Space: 46.2 MB (8.08%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,15 +1,12 @@
 class Solution:
     def maximumBeauty(self, nums: list[int], k: int) -> int:
-        # Extend the range for each element in nums
         events = []
         for num in nums:
-            events.append((num - k, 1))  # Start of range
-            events.append((num + k + 1, -1))  # End of range (exclusive)
+            events.append((num - k, 1)) 
+            events.append((num + k + 1, -1))  
 
-        # Sort events by value, and in case of tie, by type of event
         events.sort()
 
-        # Use a sweep line approach to calculate the maximum overlap
         max_beauty = 0
         current_count = 0
         for value, effect in events:
