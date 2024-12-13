a014fc8160f8d232d2cde52aadd4ad9a6b56ea53
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2024-12-13 23:50:40 +0530

    Time: 574 ms (44.37%), Space: 43.9 MB (27.65%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,24 @@
+class Solution:  
+    def findScore(self, nums: List[int]) -> int:  
+        n = len(nums)  
+        res = 0  
+        marked = set()  
+        min_heap = []  
+
+        for i in range(n):  
+            heapq.heappush(min_heap, (nums[i], i))  
+
+        while min_heap:  
+            while min_heap:  
+                value, i = heapq.heappop(min_heap)  
+                if i not in marked:  
+                    break  
+            else:  
+                break  
+
+            marked.add(i)  
+            if i - 1 >= 0: marked.add(i - 1)  
+            if i + 1 < n: marked.add(i + 1)  
+            res += value  
+
+        return res
