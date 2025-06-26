6993a54697d5fe4c44b34ccf1a32b6e7d99d8d74
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2025-06-27 00:40:22 +0530

    Time: 3 ms (86.01%), Space: 19.5 MB (29.93%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,9 +1,18 @@
 class Solution:
+    #def minimumTotal(self, tri: List[List[int]]) -> int:
+    #    n = len(tri)
+    #    @lru_cache(None)
+    #    def help(i, j):
+    #        if i == n-1:
+    #            return tri[i][j]
+    #        return tri[i][j] + min(help(i+1,j), help(i+1,j+1))
+    #    return help(0,0)
+
     def minimumTotal(self, tri: List[List[int]]) -> int:
         n = len(tri)
-        @lru_cache(None)
-        def help(i, j):
-            if i == n-1:
-                return tri[i][j]
-            return tri[i][j] + min(help(i+1,j), help(i+1,j+1))
-        return help(0,0)
+        dp = [[-1]*len(tri[-1]) for _ in range(n)]
+        dp[n-1] = tri[-1]
+        for i in range(n-2, -1, -1):
+            for j in range(len(tri[i])):
+                dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + tri[i][j]
+        return dp[0][0]
